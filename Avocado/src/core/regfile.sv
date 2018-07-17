5501cebcd08eca3febd1e0981147769cbe72d159
YauS <yaus@users.noreply.github.com>
2018-07-17 22:06:06 +0800

    Change syntax

--- Avocado/src/core/regfile.sv
+++ Avocado/src/core/regfile.sv
@@ -24,11 +24,10 @@ for(i=1;i<32;i++) begin:reg_x
 end
 endgenerate
 
-always_comb @(*) begin
-    int j;
+always_comb begin
     reg_rs1_out = 32'h0;
     reg_rs2_out = 32'h0;
-    for(j=1;j<32;j++) begin
+    for(int j=1;j<32;j++) begin
         if(rf_intf.reg_sel_rs1[j])
             reg_rs1_out |= x[j];
         if(rf_intf.reg_sel_rs2[j])
